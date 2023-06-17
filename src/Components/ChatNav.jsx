cef8fb343bedbc87ecefc04eee799b012cb3fe6b
abdqudus <abdkhudusadeberyo@gmail.com>
2023-06-17 18:54:52 +0100

    Added a sign out feature

--- src/Components/ChatNav.jsx
+++ src/Components/ChatNav.jsx
@@ -84,7 +84,6 @@ const ChatNav = () => {
       lastSeenString = `last seen on ${day}  `;
     }
   }
-
   return (
     <>
       <nav>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../Contexts/AuthContext";
 import Status from "../img/status.png";
 import WhiteStatus from "../img/status-white.png";
 import MoreWhite from "../img/more-white.png";
 import defaultDP from "../img/user.png";
 import More from "../img/more.png";
+import { useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
+import { mobileDeviceChatContext } from "../Contexts/ShowMobileDeviceChat";
 const Navbar = ({ showUserProfile }) => {
+  const navigate = useNavigate();
+  const { width } = useContext(mobileDeviceChatContext);
   const { currentUser } = useContext(AuthContext);
+  const [showSignOut, setShowSignOut] = useState(false);
+  const handleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth);
+    navigate("login");
+  };
 
   return (
     <nav className="navbar">
-      <div>
+      <div style={{ position: "relative" }}>
         <img
           className="dp"
           onClick={() => {
@@ -23,12 +34,26 @@ const Navbar = ({ showUserProfile }) => {
 
       <img src={Status} className=" big-navbar-icon" alt="status icon" />
       <img src={WhiteStatus} className="small-navbar-icon" alt="status icon" />
-      <img src={More} className="big-navbar-icon" alt="more options icon" />
       <img
+        src={More}
+        className="big-navbar-icon"
+        alt="more options icon"
+        onClick={() => setShowSignOut((prev) => !prev)}
+      />
+      <img
+        onClick={() => setShowSignOut((prev) => !prev)}
         className="small-navbar-icon"
         src={MoreWhite}
         alt="more options icon"
       />
+      <div
+        onClick={handleSignOut}
+        className={`logout-div ${showSignOut ? "" : "hidden"} ${
+          width <= 600 ? "small-signout" : ""
+        }`}
+      >
+        <p>Sign Out</p>
+      </div>
     </nav>
   );
 };
